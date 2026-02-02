a01033072ed39c1a28ba6aca5d370cc94c200915
Mattias Svensson <mattsve@users.noreply.github.com>
2026-02-02 11:31:36 +0100

    chordal hold handedness

--- keyboards/cyboard/imprint/imprint_function_row_5key_bottom_row/keymaps/vial/keymap.c
+++ keyboards/cyboard/imprint/imprint_function_row_5key_bottom_row/keymaps/vial/keymap.c
@@ -4,6 +4,17 @@
 
 #include QMK_KEYBOARD_H
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = 
+    LAYOUT_fun_full_bottom_row(
+            'L', 'L', 'L', 'L', 'L', 'L',                            'R', 'R', 'R', 'R', 'R', 'R',
+            'L', 'L', 'L', 'L', 'L', 'L',                            'R', 'R', 'R', 'R', 'R', 'R',
+            'L', 'L', 'L', 'L', 'L', 'L',                            'R', 'R', 'R', 'R', 'R', 'R',
+            'L', 'L', 'L', 'L', 'L', 'L',                            'R', 'R', 'R', 'R', 'R', 'R',
+            'L', 'L', 'L', 'L', 'L', 'L',                            'R', 'R', 'R', 'R', 'R', 'R',
+            'L', 'L', 'L', 'L', 'L', '*', '*', '*',        '*', '*', '*', 'R', 'R', 'R', 'R', 'R',
+                                     '*', '*', '*',        '*', '*', '*'
+        );
+        
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_fun_full_bottom_row(
